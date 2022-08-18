5400cf3a9de546df543ddfc85797c1acc407e00b
alex kimathi <keemalekie@gmail.com>
2022-08-18 22:11:48 +0300

    commit tree 41daefca03a00ecdb902273f49d56fe1f47b7cb0

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    
+    names.map{|name|  "Hello, my name is #{name}."}
+end
+
+def assign_rooms(names)
+    message = []
+    names.each.with_index(1) do|name, index|
+       message << "Hello, #{name}! You'll be assigned to room #{index}!"
+    end
+     message
+end
+def printer(names)
+     batch_badge_creator(names).each {|person| puts person}
+    assign_rooms(names).each {|person| puts person}
+end 
+printer(["Alex","kim","kamau","murithi"])
